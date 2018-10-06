ff09f4558e955c2e277a938257074d4db761e97d
KomalKadukar <kkadukar@gmail.com>
2018-10-06 16:51:19 -0500

    Agged Tag Model and ran db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_06_214217) do
+ActiveRecord::Schema.define(version: 2018_10_06_215010) do
 
   create_table "speakers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

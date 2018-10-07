f09b33ea997da2a05c3b0ed93ded0ab148d9c52d
KomalKadukar <kkadukar@gmail.com>
2018-10-07 16:21:31 -0500

    Ran db migration for created tedtalk model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_06_215010) do
+ActiveRecord::Schema.define(version: 2018_10_07_211941) do
 
   create_table "speakers", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,17 @@ ActiveRecord::Schema.define(version: 2018_10_06_215010) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tedtalks", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "url"
+    t.integer "views"
+    t.integer "speaker_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["speaker_id"], name: "index_tedtalks_on_speaker_id"
   end
 
 end

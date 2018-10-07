6a055358ee8c1de832d59c8e0f6154d2b6ad0a4a
KomalKadukar <kkadukar@gmail.com>
2018-10-07 16:56:08 -0500

    Correct typo for tedtalk model and added seed to load data

--- app/models/tedtalk.rb
+++ app/models/tedtalk.rb
@@ -1,7 +1,7 @@
 class Tedtalk < ApplicationRecord
   belongs_to :speaker
   has_many :categories
-  has_many :tags, though: :categories
+  has_many :tags, through: :categories
 
   validates :title, :description, :url, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'Ted_Talks_Data.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1', skip_blanks: true)
+
+Category.destroy_all
+Tag.destroy_all
+Speaker.destroy_all
+Tedtalk.destroy_all
+
+csv.each do |row|
+  # puts row.to_hash
+  speaker = Speaker.find_or_create_by(name: row['main_speaker'])
+
+  talk = Tedtalk.create( title:  row['title'],
+    description: row['description'],
+    url:         row['url'],
+    views:       row['views'],
+    speaker:     speaker)
+
+  (row['tags']).split(",").map { |s|
+    tag = Tag.find_or_create_by(name: s)
+
+    category = Category.create(tag: tag, tedtalk: talk)
+  }
+
+  puts "#{talk.title} saved"
+
+end
+
+puts "There are now #{Speaker.count} rows in Speaker table"
+puts "There are now #{Tag.count} rows in Tag table"
+puts "There are now #{Tedtalk.count} rows in tedtalk table"
+puts "There are now #{Category.count} rows in Category table"

500a7da52fba9d529aedc5bc42f39f6075806319
KomalKadukar <kkadukar@gmail.com>
2018-10-08 15:07:39 -0500

    Added new pages controller and about html page to display about page for the site.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   # get 'tedtalks/index'
   # get 'tedtalks/show'
 
+  # root_path
   root to: 'tedtalks#index'
 
+  # about page path
+  get 'about', to: 'pages#about', as: 'about'
+
+  # tedtalk path - index page
   resources :tedtalks, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

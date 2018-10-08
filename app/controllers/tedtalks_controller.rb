b5151987d20c0c4afe8702262ff8e151a5a30050
KomalKadukar <kkadukar@gmail.com>
2018-10-08 14:46:49 -0500

    Added new tedtalks controller and updated routes file to add controller path

--- app/controllers/tedtalks_controller.rb
+++ app/controllers/tedtalks_controller.rb
@@ -1,7 +1,9 @@
 class TedtalksController < ApplicationController
   def index
+    @tedtalks = Tedtalk.all
   end
 
   def show
+    @tedtalk = Tedtalk.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'tedtalks/index'
-  get 'tedtalks/show'
+  # get 'tedtalks/index'
+  # get 'tedtalks/show'
+
+  root to: 'tedtalks#index'
+
+  resources :tedtalks, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
